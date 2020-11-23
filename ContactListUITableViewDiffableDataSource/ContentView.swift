deddeb6fd2802131eccef8fda13bfab5fe6bb3f9
Dawid Kubicki <dawid@pluscode.io>
2020-11-23 16:58:02 +0100

    some init ui

--- ContactListUITableViewDiffableDataSource/ContentView.swift
+++ ContactListUITableViewDiffableDataSource/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+class DiffableTableViewController: UITableViewController {
+    
+    //UITableViewDiffableDataSource
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Contacts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
     }
+    
+}
+
+struct DiffableContainer: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        UINavigationController(rootViewController:
+                                DiffableTableViewController(style: .insetGrouped))
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
+    }
+    typealias UIViewControllerType = UIViewController
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        DiffableContainer()
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
     }
 }
