ac8be3661a5aeadde5f6ad4a1b4ce340dbc2e182
Dawid Kubicki <dawid@pluscode.io>
2020-11-23 16:39:27 +0100

    Initial Commit

---
+++ ContactListUITableViewDiffableDataSource/ContactListUITableViewDiffableDataSourceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListUITableViewDiffableDataSourceApp.swift
+//  ContactListUITableViewDiffableDataSource
+//
+//  Created by Dawid Kubicki on 23/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ContactListUITableViewDiffableDataSourceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
